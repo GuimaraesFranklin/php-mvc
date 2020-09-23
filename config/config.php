bb43a62b7b4d729b88049a4c293c203a2d7d104f
Franklin Guimarães <franklincassiano@gmail.com>
2020-09-23 13:29:17 -0300

    'Configuração de diretórios'

--- config/config.php
+++ config/config.php
@@ -2,4 +2,24 @@
 #Diretorios Raizes
 $PastaInterna = "";
 
-define('DIRPAGE', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
+define('DIRPAGE__', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}"); #Caminho Variavel Aplicação Host
+
+if (substr($_SERVER['DOCUMENT_ROOT'], -1) == '/') {
+
+    define('DIRREQ__', "{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}"); #Caminho Fisico Aplicação no Server
+} else {
+    define('DIRREQ__', "{$_SERVER['DOCUMENT_ROOT']}/{$PastaInterna}");
+}
+
+#Diretórios especificos
+define('DIRIMG__', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}public/img/");
+define('DIRCSS__', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}public/css/");
+define('DIRJS__', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}public/js/");
+define('DIRFONTS__', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}public/fonts/");
+define('DIRADMIN__', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}public/admin/");
+
+#Acesso DataBase
+define('HOST', "localhost");
+define('DB', "sistema");
+define('USER', "root");
+define('PASS', "password");
--- public/index.php
+++ public/index.php
@@ -3,4 +3,4 @@ header("Content-Type: text/html; charset=utf8");
 require_once("../config/config.php");
 require_once("../src/vendor/autoload.php");
 
-echo DIRPAGE;
+echo DIRIMG__;
