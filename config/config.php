07cb49c51623fd8fea7765f4f1c1172240740505
Franklin Guimarães <franklincassiano@gmail.com>
2020-09-23 12:59:07 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+#Diretorios Raizes
+$PastaInterna = "";
+
+define('DIRPAGE', "http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-Type: text/html; charset=utf8");
+require_once("../config/config.php");
+require_once("../src/vendor/autoload.php");
+
+echo DIRPAGE;
